390ac5ea7cf7bde04f703a8b79b41460cd7dcaf7
Silva Martins <silvaneimartins_rcc@hotmail.com>
2026-04-21 20:49:23 -0300

    feat: created mines and clock

---
+++ src/mines.c
@@ -0,0 +1,79 @@
+#include "mines.h"
+#include "load_media.h"
+
+bool mines_new(struct Mines **mines, SDL_Renderer *renderer)
+{
+    *mines = calloc(1, sizeof(struct Mines));
+    if (!*mines)
+    {
+        fprintf(stderr, "Error in callon of new clock. \n");
+        return false;
+    }
+
+    struct Mines *m = *mines;
+
+    m->renderer = renderer;
+
+    m->back_dest_rects.x = DIGIT_BACK_LEFT * 2;
+    m->back_dest_rects.y = DIGIT_BACK_TOP * 2;
+    m->back_dest_rects.w = DIGIT_BACK_WIDTH * 2;
+    m->back_dest_rects.h = DIGIT_BACK_HEIGHT * 2;
+    m->digit_width = DIGIT_WIDTH * 2;
+    m->digit_height = DIGIT_HEIGHT * 2;
+
+    if (!load_media_sheet(m->renderer, m->back_image, "images/digitback.png", DIGIT_BACK_WIDTH, DIGIT_BACK_HEIGHT, &m->back_src_rects))
+    {
+        return false;
+    };
+
+    if (!load_media_sheet(m->renderer, m->digit_image, "images/digits.png", DIGIT_WIDTH, DIGIT_HEIGHT, &m->digit_src_rects))
+    {
+        return false;
+    }
+
+    return true;
+}
+
+void mines_free(struct Mines **mines)
+{
+    if (*mines)
+    {
+        struct Mines *m = *mines;
+
+        if (m->digit_src_rects)
+        {
+            free(m->digit_src_rects);
+            m->digit_src_rects = NULL;
+        }
+
+        if (m->digit_image)
+        {
+            SDL_DestroyTexture(m->digit_image);
+            m->digit_image = NULL;
+        }
+
+        if (m->back_src_rects)
+        {
+            free(m->back_src_rects);
+            m->back_src_rects = NULL;
+        }
+
+        if (m->back_image)
+        {
+            SDL_DestroyTexture(m->back_image);
+            m->back_image = NULL;
+        }
+
+        m->renderer = NULL;
+
+        free(m);
+        m = NULL;
+        *mines = NULL;
+
+        printf("Mines clean. \n");
+    }
+}
+
+void mines_draw(const struct Mines *m)
+{
+}
---
+++ src/mines.h
@@ -0,0 +1,23 @@
+#ifndef MINES_H
+#define MINES_H
+
+#include "main.h"
+
+struct Mines
+{
+    SDL_Renderer *renderer;
+    SDL_Texture *back_image;
+    SDL_Texture *digit_image;
+    SDL_FRect *back_src_rects;
+    SDL_FRect *digit_src_rects;
+    SDL_FRect *back_dest_rects;
+    float digit_width;
+    float digit_height;
+}
+
+bool
+mines_new(struct Mines **mines, SDL_Renderer *renderer);
+void mines_free(struct Mines **mines);
+void mines_draw(const struct Mines *m);
+
+#endif
